66541b1fdee9e779d10de4611abf0364c64e6cbb
Prateek Tomar <prateek.tom111@gmail.com>
2018-03-06 00:11:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/ui/main.js',function(req,res){
+   res.sendFile(path.join(__dirname,'ui','main.js')); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
