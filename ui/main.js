5e2385e1b8ef8a6fed90e8f2d955a17e7a832f30
Prateek Tomar <prateek.tom111@gmail.com>
2018-03-06 00:09:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,22 @@
 console.log('Loaded!');
-var element= document.getElementById("madi");
+//counter code
+var button=document.getElementById('counter');
+
+button.onclick=function(){
+    //make a request to counter end point
+    var request=new XMLHttpRequest();
+    //capture respose and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    
+    //render the variable in correct span
+  request.open('GET','http://prateektom111.imad.hasura-app.io/counter',true);
+  request.send(null);
+};
